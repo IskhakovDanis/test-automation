69571bc0fadd211fe7e48f1f4b4f78f615ee5da4
diskh <piese_everyine@mail.ru>
2024-03-23 20:39:37 +0300

    allure report add

--- tests/test_input_and_click_page.py
+++ tests/test_input_and_click_page.py
@@ -3,9 +3,13 @@
 from pytest import mark
 
 
-@mark.homework
+#@mark.homework
 def test_input_and_click(input_and_click_page):
     input_and_click_page.input_text()
     input_and_click_page.click_add_btn()
     input_and_click_page.check_items()
     input_and_click_page.click_dlt_btn()
+
+@mark.homework
+def test_1():
+    pass
